499383e9f61778212ea7f335be0e32a13ecfefe2
Leon Windhorst <leon@windhorst.dev>
2024-05-14 11:23:17 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,33 @@
-function Write-GitBranchName {
+Set-PSReadLineOption -Colors @{ InlinePrediction = "DarkGray" }
+
+function Write-CurrentWorkingDirectory {
+    Write-Host $(Convert-Path $(Get-Location)) -NoNewline -ForegroundColor Blue
+}
+
+function Write-GitBranch {
     $branch = $(git rev-parse --abbrev-ref HEAD)
 
     if ($branch) {
-        Write-Host " git:(" -NoNewline -ForegroundColor green
-        Write-Host $branch -NoNewline -ForegroundColor red
-        Write-Host ")" -NoNewline -ForegroundColor green
+        Write-Host " git:(" -NoNewline -ForegroundColor Green
+        Write-Host $branch -NoNewline -ForegroundColor Red
+        Write-Host ")" -NoNewline -ForegroundColor Green
     }
 }
 
-function Prompt {
-    Write-Host $(Convert-Path $(Get-Location)) -NoNewline -ForegroundColor blue
+function Write-PromptSymbol {
+    Write-Host "→" -NoNewline -ForegroundColor Green
+}
 
-    Write-GitBranchName
-
+function Write-NewLine {
     Write-Host ""
-    Write-Host "→" -NoNewline -ForegroundColor green
+}
+
+function Prompt {
+    Write-CurrentWorkingDirectory
+    Write-GitBranch
+    Write-NewLine
+
+    Write-PromptSymbol
+
     return " "
 }
